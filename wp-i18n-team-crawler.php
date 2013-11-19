ac0b1cc07afcb2e508b8fe907d9c3d17483a5302
Marko Heijnen <info@markoheijnen.com>
2013-11-19 16:10:35 +0100

    Init version

---
+++ wp-i18n-team-crawler.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: WP I18N Team Crawler
+Plugin URI:  
+Description: Scans through a few APIs to generate a list of all languages/members
+Version:     0.1
+License:     GPLv2 or later
+Author:      Marko Heijnen
+Author URI:  http://www.markoheijnen.com
+Text Domain: wp-i18n-team-crawler
+Domain Path: /languages
+*/
+
+
+class WP_I18n_Team_Crawler {
+
+}
+
+new WP_I18n_Team_Crawler();
