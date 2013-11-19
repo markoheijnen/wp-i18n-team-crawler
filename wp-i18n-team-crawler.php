8b6cc5ba2f0055f7b482cbaad31178709972d4a8
Marko Heijnen <info@markoheijnen.com>
2013-11-19 16:12:30 +0100

    Change plugin name and added empty crawler file

--- wp-i18n-team-crawler.php
+++ wp-i18n-team-crawler.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: WP I18N Team Crawler
+Plugin Name: WP I18N Teams
 Plugin URI:  
 Description: Scans through a few APIs to generate a list of all languages/members
 Version:     0.1
@@ -11,9 +11,10 @@ Text Domain: wp-i18n-team-crawler
 Domain Path: /languages
 */
 
+include 'crawler.php';
 
-class WP_I18n_Team_Crawler {
+class WP_I18n_Teams {
 
 }
 
-new WP_I18n_Team_Crawler();
+new WP_I18n_Teams();
