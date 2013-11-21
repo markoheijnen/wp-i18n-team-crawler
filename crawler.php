4dd18f62503afb441c9edbd22fc9cd5b2d8614cf
Marko Heijnen <info@markoheijnen.com>
2013-11-21 16:32:06 +0100

    Add ability to retrieve all WordPress languages by using the locales.php file from GlotPress

---
+++ crawler.php
@@ -0,0 +1,29 @@
+<?php
+
+class WP_I18n_Team_Crawler {
+
+	public static function get_sites() {
+		include 'lib/locales.php';
+
+		$locales = GP_Locales::locales();
+		$locales = array_filter( $locales, array( __CLASS__, 'filter_locale_for_wp' ) );
+		usort( $locales, array( __CLASS__, 'sort_locales' ) );
+
+		return $locales;
+	}
+
+	public static function filter_locale_for_wp( $element ) {
+		if ( ! isset( $element->wp_locale ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public static function sort_locales( $a, $b ) {
+		return strcmp( $a->english_name, $b->english_name );
+	}
+
+}
+
+new WP_I18n_Team_Crawler();
