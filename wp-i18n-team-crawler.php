692edcc304a8d558c2d3d25ba157c1227b25777c
Marko Heijnen <info@markoheijnen.com>
2014-05-26 01:30:00 +0200

    Add counts to top of the table. Fixes #4

--- wp-i18n-team-crawler.php
+++ wp-i18n-team-crawler.php
@@ -20,24 +20,53 @@ class WP_I18n_Teams {
 	}
 
 	public function all_information( $args ) {
+		$no_sites = $no_downloads = $latest = $minor_behind = $major_behind_one = $major_behind_many = 0;
+
 		$sites = WP_I18n_Team_Crawler::get_sites();
 
-		$html  = '<p>' . __( '(white: none; green: okay; yellow: minor version behind, orange: 1 major version behind, red: many major version behind)', 'wp-i18n-team-crawler' ) . '</p>';
-		$html .= '<table>';
-		$html .= '<thead>';
-		$html .= '<tr>';
-		$html .= '<th>' . __( 'Locale Name', 'wp-i18n-team-crawler' ) . '</th>';
-		$html .= '<th>' . __( 'Native Name', 'wp-i18n-team-crawler' ) . '</th>';
-		$html .= '<th>' . __( 'Locale Code', 'wp-i18n-team-crawler' ) . '</th>';
-		$html .= '<th>' . __( 'WordPress Locale', 'wp-i18n-team-crawler' ) . '</th>';
-		$html .= '<th>' . __( 'Version', 'wp-i18n-team-crawler' ) . '</th>';
-		$html .= '</tr>';
-		$html .= '</thead>';
-		$html .= '<tbody>';
+		$table  = '<table>';
+		$table .= '<thead>';
+		$table .= '<tr>';
+		$table .= '<th>' . __( 'Locale Name', 'wp-i18n-team-crawler' ) . '</th>';
+		$table .= '<th>' . __( 'Native Name', 'wp-i18n-team-crawler' ) . '</th>';
+		$table .= '<th>' . __( 'Locale Code', 'wp-i18n-team-crawler' ) . '</th>';
+		$table .= '<th>' . __( 'WordPress Locale', 'wp-i18n-team-crawler' ) . '</th>';
+		$table .= '<th>' . __( 'Version', 'wp-i18n-team-crawler' ) . '</th>';
+		$table .= '</tr>';
+		$table .= '</thead>';
+		$table .= '<tbody>';
 
 		foreach ( $sites as $site ) {
 			$locale = WP_I18n_Team_Crawler::get_locale( $site->slug );
-			$class  = $locale['version'] ? $this->get_version_class( $locale['version'] ) : '';
+			$class  = '';
+
+		 	if ( $locale['version'] ) {
+		 		$class = 'version';
+
+				$wp_version = WP_I18n_Team_Crawler::current_wordpress_version();
+				$one_lower  = $wp_version - 0.1;
+
+				if ( $locale['version'] == $wp_version ) {
+					$class .= ' latest';
+
+					$latest++;
+				}
+				else if ( substr( $locale['version'], 0, 3 ) == substr( $wp_version, 0, 3 ) ) {
+					$class .= ' minor-behind';
+
+					$minor_behind++;
+				}
+				else if ( substr( $locale['version'], 0, 3 ) == substr( $one_lower, 0, 3 ) ) {
+					$class .= ' major-behind-one';
+
+					$major_behind_one++;
+				}
+				else {
+					$class .= ' major-behind-many';
+
+					$major_behind_many++;
+				}
+			}
 
 			if ( $locale['url'] ) {
 				$locale_code = '<a href="' . $locale['url'] . '">' . $site->slug . '</a>';
@@ -47,53 +76,46 @@ class WP_I18n_Teams {
 				}
 				else {
 					$version = __( 'None', 'wp-i18n-team-crawler' );
+
+					$no_downloads++;
 				}
 			}
 			else {
-				$locale_code  = $site->slug;
-				$version = __( 'No site', 'wp-i18n-team-crawler' );
+				$locale_code = $site->slug;
+				$version     = __( 'No site', 'wp-i18n-team-crawler' );
+
+				$no_sites++;
 			}
 
-			$html .= '<tr class="' . $class . '">';
-			$html .= '<td>' . $site->english_name . '</td>';
-			$html .= '<td>' . $site->native_name . '</td>';
-			$html .= '<td>' . $locale_code . '</td>';
-			$html .= '<td>' . $site->wp_locale . '</td>';
-			$html .= '<td>' . $version . '</td>';
-			$html .= '</tr>';
+			$table .= '<tr class="' . $class . '">';
+			$table .= '<td>' . $site->english_name . '</td>';
+			$table .= '<td>' . $site->native_name . '</td>';
+			$table .= '<td>' . $locale_code . '</td>';
+			$table .= '<td>' . $site->wp_locale . '</td>';
+			$table .= '<td>' . $version . '</td>';
+			$table .= '</tr>';
 		}
 
-		$html .= '</tbody>';
+		$table .= '</tbody>';
+		$table .= '</table>';
 
-		$html .= '</table>';
 
-		$html = '<div class="translators-info">' . $html . '</div>';
+		$html  = '<p>';
+		$html .= sprintf(
+			__( '%s locales up-to-date. %s locales behind by one minor version. %s locales behind by one major version. %s locales behind by two or more major versions. %s locales do not yet have a package available.', 'wp-i18n-team-crawler' ),
+			'<strong class="label latest">' . $latest . '</strong>',
+			'<strong class="label minor-behind">' . $minor_behind . '</strong>',
+			'<strong class="label major-behind-one">' . $major_behind_one . '</strong>',
+			'<strong class="label major-behind-many">' . $major_behind_many . '</strong>',
+			'<strong class="label">' . ( $no_sites + $no_downloads ) . '</strong>'
+		);
+		$html .= '</p>';
+
+		$html = '<div class="translators-info">' . $html . $table . '</div>';
 
 		wp_enqueue_style( 'wp-i18n-team', plugins_url( 'css/front.css', __FILE__ ), array(), '0.6' );
 
 		return $html;
-	}
-
-	private function get_version_class( $version ) {
-		$class = 'version';
-
-		$wp_version = WP_I18n_Team_Crawler::current_wordpress_version();
-		$one_lower  = $wp_version - 0.1;
-
-		if ( $version == $wp_version ) {
-			$class .= ' latest';
-		}
-		else if ( substr( $version, 0, 3 ) == substr( $wp_version, 0, 3 ) ) {
-			$class .= ' minor-behind';
-		}
-		else if ( substr( $version, 0, 3 ) == substr( $one_lower, 0, 3 ) ) {
-			$class .= ' major-behind-one';
-		}
-		else {
-			$class .= ' major-behind-many';
-		}
-
-		return $class;
 	}
 
 }
